7267c95a1fd79f8f4be23af4b075d0c97741be5f
geoffreypi <geoffreypi@gmail.com>
2021-02-26 11:58:37 -0400

    Added nicer acceleration
     - math

--- New Unity Project/Assets/Scripts/GameManager.cs
+++ New Unity Project/Assets/Scripts/GameManager.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    public float moveSpeed { get; private set; } = 1;
+    public bool boosting = false;
+    public float maxSpeed { get; private set; } = 1f;
+    public float maxAcc { get; private set; } = 0.01f;
+    public float currentMoveSpeed { get; private set; } = 0f;
     [SerializeField] private Player player;
 
     
@@ -25,25 +28,37 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxis("Vertical")>0)
+        {
+            currentMoveSpeed += maxAcc;
+        }
+        else
+        {
+            currentMoveSpeed -= maxAcc;
+        }
+
+        currentMoveSpeed = Mathf.Clamp(currentMoveSpeed, 0f, maxSpeed);
     }
 
     private void OnSpeedBoost()
     {
-        StartCoroutine("SpeedBoost");
+        if (!boosting)
+        {
+            StartCoroutine("SpeedBoost");
+        }
     }
 
     private IEnumerator SpeedBoost()
     {
-        float elapsed = 0f;
-        float duration = 5f;
-        moveSpeed *= 2f;
-        while(elapsed < duration)
-        {
-            yield return null;
-            elapsed += Time.deltaTime;
-        }
-        moveSpeed /= 2;
+        maxSpeed *= 2f;
+        maxAcc *= 2f;
+
+        boosting = true;
+        yield return new WaitForSeconds(5f);
+        boosting = false;
+
+        maxSpeed /= 2f;
+        maxAcc /= 2f;
     }
 
 }
